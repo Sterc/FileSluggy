31f47415dace204892036e98ea7620a546cd2908
Joeke Kloosterman <joekekloosterman@gmail.com>
2016-09-23 22:59:57 +0200

    Add GPM config file + fix coding standards in plugin file

--- core/components/filesluggy/elements/plugins/filesluggy.plugin.php
+++ core/components/filesluggy/elements/plugins/filesluggy.plugin.php
@@ -2,7 +2,6 @@
 /**
  * FileSluggy by Sterc
  * Sanitizes a filename on upload to be a nice and more clean filename, so it will work better with phpthumbof, pthumb and overall cleaner filenames and directories.
- 
  * Copyright 2015 by Sterc
  * FileSluggy is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
@@ -17,17 +16,18 @@
  * formAlicious; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
  *
- *
  * @author Sterc <modx@sterc.nl>
  * @credits:
  *      - Based on the code of the sanitizefilename plugin of Benjamin Vauchel https://github.com/benjamin-vauchel/SanitizeFilename
  *      - The Slug() phunction of AlixAxel https://github.com/alixaxel/phunction/blob/master/phunction/Text.php
- * @version Version 1.2
+ * @version Version 1.3
  * @package filesluggy
  */
 
 $FileSluggy = $modx->getService('filesluggy', 'FileSluggy', $modx->getOption('filesluggy.core_path', null, $modx->getOption('core_path') . 'components/filesluggy/') . 'model/filesluggy/', $scriptProperties);
-if (!($FileSluggy instanceof FileSluggy)) return;
+if (!($FileSluggy instanceof FileSluggy)) {
+    return;
+}
 
 switch ($modx->event->name) {
     case 'OnFileManagerDirCreate':
@@ -44,10 +44,9 @@ switch ($modx->event->name) {
     case 'OnFileManagerUpload':
         $url = parse_url($_SERVER['HTTP_REFERER']);
         $query = $url['query'];
-        if (
-            strpos($query,'a=resource/create') !== false || 
-            strpos($query,'a=resource/update') !== false || 
-            strpos($query,'a=media/browser') !== false 
+        if (strpos($query, 'a=resource/create') !== false ||
+            strpos($query, 'a=resource/update') !== false ||
+            strpos($query, 'a=media/browser') !== false
         ) {
             foreach ($files as $file) {
                 if ($FileSluggy->santizeAllowThisMediaSource($source->get('id'))) {
