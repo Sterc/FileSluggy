bec0aed829ed4519bcd722b70283f9d4bed6f37a
Joeke Kloosterman <joekekloosterman@gmail.com>
2016-10-17 22:13:25 +0200

    Add realpath to the basePath variable to prevent double slashes in path (Issue#10, PR#11)

--- core/components/filesluggy/elements/plugins/filesluggy.plugin.php
+++ core/components/filesluggy/elements/plugins/filesluggy.plugin.php
@@ -37,7 +37,7 @@ switch ($modx->event->name) {
                 $basePath = $source->getBasePath();
                 $dirName  = basename($directory);
                 $dirName  = $FileSluggy->sanitizeName($dirName, true);
-                $FileSluggy->renameContainer($source, str_replace($basePath, '', $directory), $dirName);
+                $FileSluggy->renameContainer($source, str_replace(realpath($basePath), '', $directory), $dirName);
             }
         }
         break;
